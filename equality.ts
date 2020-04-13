e32b6c27480496bb3348a1341ec6244b095014dc
QWYNG <ikusawasi@gmail.com>
2020-04-13 20:41:09 +0900

    equalty

---
+++ equality.ts
@@ -0,0 +1,18 @@
+type IdDisplay = {
+    id: string,
+    display: string
+  }
+  const list: IdDisplay[] = [
+    {
+      id: 'foo',
+      display: 'Foo Select'
+    },
+    {
+      id: 'bar',
+      display: 'Bar Select'
+    },
+  ]
+  ​
+  const fooIndex = list.map(i => i.id).indexOf('foo');
+  console.log(fooIndex);
+  
